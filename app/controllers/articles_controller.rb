4e1a87828f10fa07fe9eb81b971966f06b2a8dda
yanl <liyan1990s@gmail.com>
2016-01-30 23:17:14 +0000

    fix the method name

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
     
     @article = Article.new(article_params)
     @article.save
-    redirect_to articles_show(@article)
+    redirect_to article_path(@article)
   end
   
   private 
