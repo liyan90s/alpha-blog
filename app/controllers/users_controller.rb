97d4a0869a55f40fd96ffd9343839bbe561f62f0
yanl <liyan1990s@gmail.com>
2016-02-03 07:04:38 +0000

    edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,21 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = "User account updated!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def user_params
       params.require(:user).permit(:user_name, :email, :password)
