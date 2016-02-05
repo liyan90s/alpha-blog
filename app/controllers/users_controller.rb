d17b5d2c81c8b98183452b46e2707e7c856036e4
yanl <liyan1990s@gmail.com>
2016-02-05 07:05:39 +0000

    sign in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Y-Blog #{@user.user_name}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
