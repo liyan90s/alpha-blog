3d912c384acf7ea1e2a22907c18cd6ea5aa058cd
yanl <liyan1990s@gmail.com>
2016-02-10 06:51:09 +0000

    add edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,22 @@ class CategoriesController < ApplicationController
     end
   end
   
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to categories_path
+    else
+      flash[:danger] = "Only admin can perform this action"
+      render 'edit'
+    end
+  end
+  
   private 
     def category_params
       params.require(:category).permit(:name)
